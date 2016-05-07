9bbe61faa40a84f6bc6843a70b1e9a11dbabe9f7
Anya Krupp <anyakrupp@gmail.com>
2016-05-06 18:24:50 -0700

    Added release 3, by printing the data about the hamster on the screen

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,9 @@
 #Release 1: Gather Data on Hamsters of Unknown Origin
 puts "What is the hamsters name?"
- hamster = gets.chomp
-puts "From 1 to 10, 1 being the lightest, how light of a sleeper are you?"
- sleep = gets.chomp
- sleep.to_i
+ hamster_name = gets.chomp
+puts "How active (loud during nights) is the hamster? From 1 to 10, 1 being the lightest."
+ active_level = gets.chomp
+ active_level.to_i
 puts "What is the fur color?"
  fur = gets.chomp
 puts "Do you think the hamster is good for adoption?"
@@ -14,5 +14,9 @@ age = gets.chomp
     age = nil 
   else age.to_i
   end
-
-puts
+#Release 3: Print the data
+puts "Your hamster name: #{hamster_name};
+      How active is the hamster: #{active_level};
+      Fur color: #{fur};
+      Adoption ready: #{adoption};
+      Hamster's age: #{age}."
