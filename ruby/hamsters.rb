d09f79fa82ea094dadc255938c9145f6f47534bd
Anya Krupp <anyakrupp@gmail.com>
2016-05-06 17:56:49 -0700

    created a code for release 1 to gather data on hamsters of unknown origin

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+#Release 1: Gather Data on Hamsters of Unknown Origin
+puts "What is the hamsters name?"
+ hamster = gets.chomp
+puts "From 1 to 10, 1 being the lightest, how light of a sleeper are you?"
+ sleep = gets.chomp
+puts "What is the fur color?"
+ fur = gets.chomp
+puts "Do you think the hamster is good for adoption?"
+ adoption = gets.chomp
+puts "How old is the hamster?"
+age = gets.chomp
+  if age.empty?
+    age = nil 
+  else age.to_i
+  end
