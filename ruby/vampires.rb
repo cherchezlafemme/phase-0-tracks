58497ad2e1c0fdef34b3dc6a5cb7ebf211c960f0
Anya Krupp <anyakrupp@gmail.com>
2016-05-08 23:50:33 -0700

    Updated the task with a plot twist and allergy detection piece

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,23 +15,44 @@ until  index == employees_number
     age=gets.chomp
     puts "What year were you born?"
     year_born=gets.chomp
+      #Assign the value for right or wrong age
+            if  age.to_i==2016-year_born.to_i
+            age_detection_passed=true
+            else age_detection_failed=true
+            end
     puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
     garlic=gets.chomp
     puts "Would you like to enroll in the company’s health insurance? (yes/no)"
     insurance=gets.chomp
+    
+                # Allergy detection program
+            begin 
+        puts "List your allergies, one at a time. Once you are done, type done."
+        allergy=gets.chomp
+            if allergy=="sunshine"
+              allergy_detection_failed=true
+            end
+        end until allergy=="done" || allergy=="sunshine"
+
+
 
         #If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
-    if age.to_i == 2016-year_born.to_i && (garlic=="yes" || insurance=="yes")
-        puts "Probably not a vampire."
-          #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-    elsif age.to_i != 2016 - year_born.to_i && (garlic=="no" || insurance=="no")
-        puts "Probably a vampire."
-          #If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-    elsif age.to_i != 2016 - year_born.to_i && garlic=="no" && insurance=="no"
-        puts "Almost certainly a vampire."
-          #Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-    elsif name == "Drake Cula" || name == "Tu Fang"
+    if  allergy_detection_failed
+      puts "Probably a vampire."
+           #Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+    elsif name === "Drake Cula" || name === "Tu Fang"
         puts "Definitely vampire."
+        
+    elsif age_detection_passed && (garlic=="yes" || insurance=="yes")
+        puts "Probably not a vampire."
+          
+        #If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+    elsif age_detection_failed && garlic=="no" && insurance=="no"
+        puts "Almost certainly a vampire."  
+        #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+    elsif age_detection_failed && (garlic=="no" || insurance=="no")
+        puts "Probably a vampire."
+      
           #Otherwise, print “Results inconclusive.”
       else puts "Results inconclusive."
     
@@ -41,4 +62,8 @@ until  index == employees_number
   #Print the result at the end of the survey.
   puts vampire_survey
 index+=1
+
 end 
+
+  #Release 5: Plot Twist
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
