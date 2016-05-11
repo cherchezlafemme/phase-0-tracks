a170f60ca724089fc9da744a590e81a3669d05d9
Anya Krupp <anyakrupp@gmail.com>
2016-05-11 12:30:18 -0700

    Used map and each methods on array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,31 @@ def send_love_to_teacher
 end
 
 send_love_to_teacher { |first_name, last_name| puts "#{first_name} #{last_name}, hi how are you?" }
+
+dbc_places = ["chicago", "seattle", "dc", "san diego"]
+dog_breeds_perk = {"Huskie" => "beautiful yodels", "Irish Wolfhound" => "size", "German Shepherd" => "loyalty"}
+
+dbc_places.each do |place|
+  puts "place is #{place}"
+end
+
+dbc_places.map do |place|
+  puts "place is #{place.upcase}"
+end
+
+dbc_places.each do |place|
+  puts "(after map) place is #{place}"
+end
+
+dbc_places.map! do |place|
+  puts "place is #{place.upcase}"
+  place.upcase
+end
+
+dbc_places.each do |place|
+  puts "(after map!) place is #{place}"
+end
+
+dog_breeds_perk.each do |breed, perk|
+  puts "the breed called #{breed} is known for it's #{perk}"
+end
