d0e180ca1a647c66dc7c76f205d7e65db511ee21
Anya Krupp <anyakrupp@gmail.com>
2016-05-11 20:50:41 -0700

    Middle poit. All done exept updating the key values.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,17 +20,16 @@ puts "What is client's favorite color?"
 fav_color = gets.chomp
 fav_color.to_str
 
-puts "Does client have any pets? (yes/no)"
+begin puts "Does client have any pets? (yes/no)"
 pet_owner = gets.chomp
-
-#Convert the data to boolean and put a condition if the data was entered in a wrong way
-
-if pet_owner.downcase == "yes"
-  pet_owner = true
-elsif pet_owner.downcase == "no"
-  pet_owner = false
-else puts "Didn't get this one. Can you tell us again, if client has any pets? Please, write yes or no"
-end
+  #Convert the data to boolean and put a condition if the data was entered in a wrong way
+  if pet_owner.downcase == "yes"
+    pet_owner = true
+  elsif pet_owner.downcase == "no"
+    pet_owner = false
+  else  puts "Didn't get this one. Can you tell us again, if client has any pets? Please, write yes or no"
+end 
+end until pet_owner == true || pet_owner == false
 
 puts "Has the client specified any special requirements?"
 requirements = gets.chomp
@@ -55,5 +54,51 @@ client_profile = {
   #string
 }
 
-#Print the hash
+puts "Do you want to add any updates about client? some/none"
+update_status = gets.chomp
+update_status.to_str
+
+#Allow designer to update keys' values.
+if update_status.downcase == "some"
+  puts "Please, enter the name of category the you want to update. (If you need help, enter see the list)"
+  category = gets.chomp
+  category.to_str
+        #Offer help: list of the categories designer can update.
+        if category.downcase == "see the list" || category.downcase == "the list" || category.downcase == "list"
+          puts "Here is the list: name, age, number_of_kids, decor_theme, fav_color, pet_owner, special_requirements"
+          puts "Please, enter the name of category the you want to update."
+          new_category = gets.chomp
+          else new_category = category.to_sym
+          end
+  #TEST PRINTING     
+   p new_category
+
+
+
+
+
+#Convert the entry to the symbol class.FOR SOME REASONE NOT ALL OF THEM ARE GETTING UPDATED!!!!
+  new_category.to_sym
+  p new_category
+
+    #compare the entry with existing symbol classes
+   
+    #Update the value in this 
+    #But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) 
+    #You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+
+
+
+elsif update_status.downcase == "none"
+  puts "Awesome! Your client profile is done!"
+else puts "I guess, you are all done."
+end
+
+
+#Update the hash if changes has been made
+
+#Print the latest version of the hash
 puts client_profile
+
+#Exit the program
+puts "The end"
