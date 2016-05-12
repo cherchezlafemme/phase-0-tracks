b092edf01d9405f21d52d3afb3c32fae8b7bb67d
Anya Krupp <anyakrupp@gmail.com>
2016-05-12 09:38:22 -0700

    Fixed the issue with symbol classes. Inputs are converting to symbol

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,7 +2,6 @@
 
 puts "What is client's name?"
 name = gets.chomp
-name.to_str
 
 puts "What is client's age?"
 age = gets.chomp
@@ -14,11 +13,9 @@ number_of_kids.to_i
 
 puts "What theme would client like for his/her place?"
 decor_theme = gets.chomp
-decor_theme.to_str
 
 puts "What is client's favorite color?"
 fav_color = gets.chomp
-fav_color.to_str
 
 begin puts "Does client have any pets? (yes/no)"
 pet_owner = gets.chomp
@@ -33,8 +30,6 @@ end until pet_owner == true || pet_owner == false
 
 puts "Has the client specified any special requirements?"
 requirements = gets.chomp
-requirements.to_str
-
   
 #Feed the data to the hash
 client_profile = {
@@ -56,29 +51,19 @@ client_profile = {
 
 puts "Do you want to add any updates about client? some/none"
 update_status = gets.chomp
-update_status.to_str
 
 #Allow designer to update keys' values.
 if update_status.downcase == "some"
   puts "Please, enter the name of category the you want to update. (If you need help, enter see the list)"
   category = gets.chomp
-  category.to_str
         #Offer help: list of the categories designer can update.
         if category.downcase == "see the list" || category.downcase == "the list" || category.downcase == "list"
           puts "Here is the list: name, age, number_of_kids, decor_theme, fav_color, pet_owner, special_requirements"
           puts "Please, enter the name of category the you want to update."
-          new_category = gets.chomp
+          new_category = gets.chomp.to_sym
           else new_category = category.to_sym
           end
-  #TEST PRINTING     
-   p new_category
 
-
-
-
-
-#Convert the entry to the symbol class.FOR SOME REASONE NOT ALL OF THEM ARE GETTING UPDATED!!!!
-  new_category.to_sym
   p new_category
 
     #compare the entry with existing symbol classes
