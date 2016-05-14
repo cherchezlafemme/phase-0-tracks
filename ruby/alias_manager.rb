8b4f6da37a6cabe605820bf263f9822bb30d2ade
Anya Krupp <anyakrupp@gmail.com>
2016-05-13 20:38:07 -0700

    mid commit before writing methods for replacing letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,23 +2,39 @@
 puts 'What is your name?'
 real_name = gets.chomp
 
-#Find the index for a whitespace
-whitespace_index=real_name.index(" ")
-#Make all downcase
+def spy_name_maker (real_name)
+#Input of the method: real name of the agent. 
+#Output: new updated spy name, where vowels replaced with next vowel and consonants replaced with next in alphabet consonants. 
 
-#Break down the name into separate letters, array
+      #Find the index for a whitespace
+      whitespace_index=real_name.index(" ")
 
-#Replace all the vowels to the next vowel in 'aeiou'
+      #Make all downcase
+      real_name = real_name.downcase
 
-#Replace all the consonants to the net one in 'bcdfghjklmnpqrstvwxz'
+      #Break down the name into separate letters, array
+      letters = real_name.split("")
 
-#Assemble the new name and make it a string: .to_str
+              #Replace all the vowels to the next vowel in 'aeiou'
 
-#Add the whitespace to the same index as before
+              #Replace all the consonants to the net one in 'bcdfghjklmnpqrstvwxz'
 
-#Make first letter upcase and the letter with whitespace index + 1
-new_name[0] = new_name[0].upcase!
-new_name[whitespace_index+1] = new_name[whitespace_index+1].upcase!
+              #Address edge cases
 
-#Print new name of the agent
-puts new_name
+      #Assemble the new name and make it a string
+      new_name = letters.join("")
+      new_name = new_name.to_str
+
+      #Add the whitespace to the same index as before
+      new_name = new_name.insert(whitespace_index, " ")
+
+      #Make first letter upcase and the letter with whitespace index + 1
+      new_name[0] = new_name[0].upcase!
+      new_name[whitespace_index+1] = new_name[whitespace_index+1].upcase!
+
+      #Print new name of the agent
+      puts new_name
+
+end
+
+#Call out the method to run it in the program
