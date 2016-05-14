255b291eb7f78894672c1874508cc9547a76ec8a
Anya Krupp <anyakrupp@gmail.com>
2016-05-14 00:29:38 -0700

    Still need to solve last piece of commit program

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,28 +6,31 @@ def spy_name_maker (real_name)
 #Input of the method: real name of the agent. 
 #Output: new updated spy name, where vowels replaced with next vowel and consonants replaced with next in alphabet consonants. 
 
-      #Find the index for a whitespace
-      whitespace_index=real_name.index(" ")
+      #Swap first and last name
+      name = real_name.split
+      updated_name = name.last + (" ") + name.first
+      updated_name.to_str
 
+      #Find the index for a whitespace
+      whitespace_index=updated_name.index(" ")
       #Make all downcase
-      real_name = real_name.downcase
+      updated_name = updated_name.downcase
 
       #Replace all the vowels to the next vowel in 'aeiou'. Using gsup on a string
-      real_name.gsub!(/[aeiou]/, "a" => "e", "e" => "i", "i" => "o", "o"=> "u", "u" => "a") 
+      updated_name.gsub!(/[aeiou]/, "a" => "e", "e" => "i", "i" => "o", "o"=> "u", "u" => "a") 
       #Vowels are replaced. We have a sting with replaced vowels.
 
       #Address edge cases d=>f, h=>j, n=>p, t=>v, z=>b
-      real_name.gsub!(/[dhntz]/, "d" => "f", "h" => "j", "n" => "p", "t"=> "v", "z" => "b") 
+      #make the upcase 
+      updated_name.gsub!(/[dhntz]/, "d" => "F", "h" => "J", "n" => "P", "t"=> "V", "z" => "B") 
 
       #Break down the name into separate letters, array
-      letters = real_name.split("")
+      letters = updated_name.split("")
 
               #Replace all the consonants to the next one in alphabet 'abcdefghijklmnopqrstuvwxyz' by using map! { |letter| letter.next } 
               # except edge cases: d=>f, h=>j, n=>p, t=>v, z=>b and vowels
-
-              
-
-
+              letters.reject{ |letter| letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+              letter == "B" || letter == "V" || letter == "P" || letter == "J" || letter == "F"}.map!{ |letter| letter.next}
 
       #Assemble the new name and make it a string
       new_name = letters.join("")
