0851eeb8280566c159caf93b2d0f58ed00a6fb66
Anya Krupp <anyakrupp@gmail.com>
2016-05-20 17:02:50 -0700

    Completed paired session with Bjorn Ramos

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,63 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times do puts "Woof" end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(int2)
+      puts int2 * 7
+  end
+
+  def secret_weapon
+    puts "*gives those famous puppy eyes that no one can resist*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
+end
+
+Snoopy = Puppy.new 
+Snoopy.fetch('ball')
+Meesh = Puppy.new
+Meesh.speak(3)
+Scoobie = Puppy.new
+Scoobie.dog_years(4)
+Scoobie.secret_weapon
+
+class Hedgehog
+
+  def initialize
+    puts "Initializing new Hedgehog instance ..."
+  end
+
+  def run
+    puts "*Runs in the wheel and wakes up everyone @ night*"
+  end
+
+  def ball
+    puts "*Rolls into a ball*"
+  end
+
+end
+
+our_hedgehogs = []
+instances = 0
+  while instances < 50 
+    our_hedgehogs = our_hedgehogs.push(Hedgehog.new)
+    p our_hedgehogs
+    instances += 1
+  end
+
+
+our_hedgehogs.each do |object| 
+  object.run 
+  object.ball
 end
 
