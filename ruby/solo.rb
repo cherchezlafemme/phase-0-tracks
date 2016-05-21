47ff86eeb5ca8742f7b8bf7164425a61250f44eb
Anya Krupp <anyakrupp@gmail.com>
2016-05-21 14:34:03 -0700

    Wrote design of the class as asked in release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,16 @@
+=begin
+Class Design
+Class: Bottle_of_wine
+Attributes:
+  - Vintage
+  - Grapes
+  - Type (table, sparkling, fortified, desert)
+  - Color (red, white, rose)
+  - Origin
+  - Producer
+Methods:
+  - Age (how many years it had been aged)
+  - Give a kiss (Gives a sound of a wine kiss when you pop the cork out)
+  - Give happiness (Makes the person who is drinking it happy)
+  - Compliment the food (takes the food and makes the food you pair it with better)
+=end
