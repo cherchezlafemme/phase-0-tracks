19921cbfa19a1ca5e21f679c03502fb188a01fba
Anya Krupp <anyakrupp@gmail.com>
2016-05-21 15:52:44 -0700

    created basic user inteface that creates a new instance amount of times user wants

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -4,7 +4,7 @@ Class: Bottle of wine
 Attributes:
   - Vintage
   - Grape
-  - Type (table, sparkling, fortified, desert)
+  - Type (table, sparkling, fortified, dessert)
   - Color (red, white, rose)
   - Origin
   - Producer
@@ -21,7 +21,7 @@ attr_reader :purpose
 attr_accessor :vintage, :grapes, :type, :color, :origin, :producer
 
 def initialize(grape, type, color)
-  puts "Congratulations! One more bottle of delicious wine has been just created for you..."
+  puts "Congratulations! A bottle of delicious wine has been just created for you..."
   @vintage = 2015
   @grape = grape
   @type = type
@@ -50,7 +50,7 @@ end
 
 end  
 
-#Driver code
+=begin #Driver code
 first_bottle = Bottle_of_wine.new("Rielsing", "table", "white")
 first_bottle.purpose
 first_bottle.vintage = 2012
@@ -58,3 +58,43 @@ first_bottle.age #=> This wine has been aged for 4 years.
 first_bottle.kiss
 first_bottle.work("Oscar Wilde")
 first_bottle.compliment("honey roasted trout with green beans")
+=end
+
+#USER INTERFACE
+puts "Hello there! How many bottles of wine would you like?"
+number = gets.chomp.to_i
+bottle_count = 0
+
+loop do
+puts "What are the grapes your wine is made of?"
+grape = gets.chomp
+puts "Please, pick the type of wine: table, sparkling, fortified, dessert."
+type = gets.chomp
+  if type == ("table" || "sparkling")
+    puts "Is it rose, red or white wine?"
+    color = gets.chomp
+  elsif type == "fortified"
+    puts "Looks like you love strong stuff! What is the color of treat? Red, white, something in between?"
+    color = gets.chomp
+  elsif type == "dessert"
+    puts "Yep, you are no different, you also have a sweet tooth! Please, tell us what is the color of your dessert?"
+    color = gets.chomp
+  else color = "Not sure... will tell you once it is in the glass!"
+  end
+
+bottle = Bottle_of_wine.new(grape, type, color)  
+
+bottle_count +=1
+  if bottle_count == 1
+    puts "I hope you are happy! You have #{bottle_count} bottle of wine"
+      if bottle_count == number
+      else puts "Let's keep going... we are off to the next bottle!"
+      end
+    else 
+    puts "I hope you are happy! You have #{bottle_count} bottles of wine"
+      if bottle_count == number
+      else puts "Let's keep going... we are off to the next bottle!"
+      end
+  end
+break if bottle_count == number
+end 
