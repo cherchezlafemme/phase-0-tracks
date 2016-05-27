e747714bc0afd6a7f642602e03ce8969c4bf204f
Anya Krupp <anyakrupp@gmail.com>
2016-05-26 20:59:46 -0700

    Added first version of the pseudocode of the program

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+// Create a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+// Input: an array of strings
+// Output: a string that is the longest from this array
+
+// Pseudocode:
+// Start the function that takes an array of strings.
+// Find out how many strings the given array has.
+// For each of the strings count the amount of letters and spaces inside them.
+// Compare this number of letter/spaces between each other and find out which one is the longest.
+// Return the one that is the longest.
+
+
+
