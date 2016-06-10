6a80816598684f6ed5f052ce0c5fda69bf7d4ca6
Anya Krupp <anyakrupp@gmail.com>
2016-06-09 22:40:43 -0700

    Initial commit of jquery doc

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,7 +1,6 @@
 // class="reader_talk" gets bigger text, changes color when the mouse is over it
 // class="writer_answer" gets a different color when mouse is over it
 // id="intro" becomes smaller once you scroll down
-
 // id="main_story" appears
 
 // id="collaps" Make the word world disappear.
@@ -13,3 +12,10 @@
 
 // id="conclusion" Reveal only at the end
 // id="fly_away" Hides the fly image from the page
+
+$(document).ready(function(){
+  $(".reader_talk").click(function(){
+    $(this).css("background-color", "#989898")
+  });
+
+});
